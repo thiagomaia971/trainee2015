c5140cc3c4e0fff8bd6d590b6bac1984a5defc50
Pedro Pisandelli <pisandelli@gmail.com>
2015-07-29 20:46:24 -0300

    Código python para o primeiro DOJO

---
+++ dojo_buraco_letras/python/dojo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+i = 0
+letras = {'A':1,'B':2,'D':1,'O':1,'P':1,'Q':1,'R':1}
+palavra = "INCONSTITUCIONALISSIMAMENTE"
+
+for letra in palavra:
+    if letra in letras:
+        i = i + letras[letra]
+
+print('Existem ', i,'buracos em ', palavra)
