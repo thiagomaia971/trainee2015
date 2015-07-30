e7e3ebe9703022d5e1dbe7c1ad9c60bcd4f131e1
Antonio Gomes de Castro Junior <agcjunior@gmail.com>
2015-07-30 13:39:43 -0300

    Exemplo com C#

---
+++ dojo_buraco_letras/CSharp/ContadorDeBuracos.Testes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exemplo.Testes
+{
+    [TestClass]
+    public class Com_Um_Contador_De_Buracos
+    {
+        [TestMethod]
+        public void Quando_Passo_A_Palavra_ABELHA_Recebo_4()
+        {
+            var cb = new ContadorDeBuracos();
+            var palavra = "ABELHA";
+            var resultado = cb.Contar(palavra);
+            Assert.AreEqual(4, resultado);
+        }
+
+        [TestMethod]
+        public void Quando_Passo_Nulo_Recebo_0()
+        {
+            var cb = new ContadorDeBuracos();
+            string palavra = null;
+            var resultado = cb.Contar(palavra);
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void Quando_Passo_Numeros_Recebo_0()
+        {
+            var cb = new ContadorDeBuracos();
+            string palavra = "123";
+            var resultado = cb.Contar(palavra);
+            Assert.AreEqual(0, resultado);
+        }
+    }
+}
---
+++ dojo_buraco_letras/CSharp/ContadorDeBuracos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Exemplo
+{
+    public class ContadorDeBuracos
+    {
+        Dictionary<char, int> buracos = new Dictionary<char, int>() { 
+                { 'A', 1 }, { 'B', 2 }, { 'D', 3 }, { 'O', 4 }, { 'P', 1 }, { 'Q', 1 }, {'R', 1 }
+            };
+
+        int contadorBuracos = 0;
+        int qtd = 0;
+
+        public int Contar(string palavra)
+        {
+            if (string.IsNullOrEmpty(palavra)) return 0;
+
+            foreach (char letra in palavra)
+            {
+                buracos.TryGetValue(letra, out qtd);
+                contadorBuracos += qtd;
+            }
+            return contadorBuracos;
+        }
+
+    }
+}
