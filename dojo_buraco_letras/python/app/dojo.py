ada59d2fab9eac52426f2b04e0baaabe84d75c6f
Pedro Pisandelli <pisandelli@gmail.com>
2015-07-30 15:44:15 -0300

    enxugando código

--- dojo_buraco_letras/python/app/dojo.py
+++ dojo_buraco_letras/python/app/dojo.py
@@ -5,15 +5,10 @@ class ContaBuraco():
         i = 0
         letras = {'A':1,'B':2,'D':1,'O':1,'P':1,'Q':1,'R':1}
 
-        #int nao sao iteraveis
-        if (isinstance(palavra, int)):
-            palavra = str(palavra)
-
-        if (palavra == None):
+        if (isinstance(palavra, str)):
+            for letra in palavra:
+                if letra in letras:
+                    i = i + letras[letra]
+            return i
+        else:
             return 0
-
-        for letra in palavra:
-            if letra in letras:
-                i = i + letras[letra]
-
-        return i
