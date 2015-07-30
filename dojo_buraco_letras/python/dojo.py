43c71dd0cc6f878d4f1936e2c544671e328737af
Pedro Pisandelli <pisandelli@gmail.com>
2015-07-29 23:30:01 -0300

    Melhorando o código

--- dojo_buraco_letras/python/dojo.py
+++ dojo_buraco_letras/python/dojo.py
@@ -2,8 +2,7 @@
 
 i = 0
 letras = {'A':1,'B':2,'D':1,'O':1,'P':1,'Q':1,'R':1}
-palavra = input("Insira uma palavra: ")
-palavra = palavra.upper()
+palavra = input("Insira uma palavra: ").upper()
 
 for letra in palavra:
     if letra in letras:
